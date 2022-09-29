2b55dfb0061f11f9bd921646815c397617b9deeb
Caroline Barbosa <bo.varig@gmail.com>
2022-09-29 20:36:36 -0300

    I inserted the json file in the url

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
   var books;
   $.ajax({
-    url:"",
+    url:"books.json",
   }).done(function(data){
     books = data.books;
+    let div = $('#content');
+
+    for(let i = 0;i <= books.length; i++){
+      $(div).append(`
+        <div class="card">
+          <img src="${books[i].img}">
+        </div>  
+      `)
+    }
   });
-
-  let div = $('#content');
-
-  for(let i = 0;i <= books.length; i++){
-    $(div).append(`
-      <div class="card">
-        <img src="${books[i].img}" >
-      </div>  
-    `)
-  }
 })
