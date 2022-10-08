c2ceb6e91d4ab8b0beaf3460493203df831c320e
Carol Vilar <cbarbosavilar@gmail.com>
2022-10-08 16:12:28 -0300

    changed the html structure of sections on home and about us pages

--- public/js/glider.js
+++ public/js/glider.js
@@ -1,9 +1,9 @@
-document.querySelectorAll('.glider').forEach(element => {
+document.querySelectorAll('.glider').forEach((element, index) => {
   new Glider(element, {
     slidesToShow: 5,
     slidesToScroll: 2,
     draggable: true,
-    dots: '.dots',
+    dots: '#dots' + index,
     arrows: {
       prev: '.glider-prev',
       next: '.glider-next'
