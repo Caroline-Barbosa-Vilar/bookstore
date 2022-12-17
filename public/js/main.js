7d33703d69e3ac0e17f1a7bf2ed335851d0aae82
Carol Vilar <bo.varig@gmail.com>
2022-12-17 20:18:29 -0300

    first atempt to display items on the screen considering change the glider lib

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,27 @@
 let books = [] 
 const endPointAPI = 'https://caroline-barbosa-vilar.github.io/bookstore-json/data/books.json'
 getApiBooks()
+const bookInsertElement = document.getElementById('items')
 
 async function getApiBooks() {
   const res = await fetch(endPointAPI)
   books = await res.json()
-} 
+  insertItemsOnScreen(books)
+}
+
+function insertItemsOnScreen(bookItems){
+  bookItems.forEach(book => {
+    bookInsertElement.innerHTML += `
+    <div>
+      <img src="${book.img}" alt="placeholder img" class="glider-image-sz">
+      <h4 class="title-test">${book.title}</h4>
+      <p>${book.synopsis}</p>
+      <div>
+        <p>$0.00</p>
+        <a href="#" class="glider-carousel-btn">Buy</a>
+      </div>
+    /div>
+    `
+  });
+}
+books
